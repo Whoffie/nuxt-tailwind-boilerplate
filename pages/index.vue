3169bbb2d56c78f84e58e906cde530f5a0de88ce
whoffie <whoffman1031@gmail.com>
2022-12-02 15:56:59 -0600

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1 class="font-sans text-3xl font-bold underline">Testing! {{message}}</h1>
+    <comp></comp>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "Hello world!"
+        }
+    }
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,vue}",
+    "./components/**/*.{js,vue}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
